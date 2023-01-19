853faaffd7783a27213bf731f79ed290be4bc013
ummu-salma <66861840+ummu-salma@users.noreply.github.com>
2023-01-19 13:05:14 +0530

    Update DB_backup.sh

--- scripts/DB_backup.sh
+++ scripts/DB_backup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+## This script is to take backup of a postgres Database from a container and then push it to S3 bucket.
+
 #Configuration
 CONTAINER_NAME="container_name"
 FILENAME="db_name-`date +%Y-%m-%d`.dump"
 DUMPS_FOLDER="/home/ubuntu/backupdb/"
 
 #Backup DB from docker container
-#docker exec $CONTAINER_NAME sh -c "PGPASSWORD=\"\$POSTGRES_PASSWORD\" pg_dump --username=\$POSTGRES_USER \$POSTGRES_USER > /tmp/$FILENAME"
 docker exec $CONTAINER_NAME sh -c "pg_dump --username=db_username -Fc db_name > /tmp/$FILENAME"
 docker cp $CONTAINER_NAME:/tmp/$FILENAME $DUMPS_FOLDER
 docker exec $CONTAINER_NAME sh -c "rm /tmp/$FILENAME"
